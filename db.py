8fe9b7aee901a02700d3a7c978950dae582bc8d1
molfaryura <oleksandrr1996@protonmail.com>
2023-04-23 17:01:05 +0300

    Issue #5: Created PasswordManagerDatabase class

--- db.py
+++ db.py
@@ -5,137 +5,117 @@ from psycopg2 import sql
 
 from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
-CUR = None
-CONN = None
 
-def connect_to_db():
-    """Connects to the postgresql database
+class PasswordManagerDatabase():
+    def __init__(self) -> None:
+        """Connects to the postgresql database"""
 
-    Returns: None
-    """
+        self.connect_to_db = psycopg2.connect(host=DB_HOST,
+                                dbname=DB_NAME,
+                                user=DB_USER,
+                                password=DB_PASSWORD,
+                                port=DB_PORT)
 
-    global CUR, CONN
+        self.db_cursor = self.connect_to_db.cursor()
 
-    CONN = psycopg2.connect(host=DB_HOST,
-                            dbname=DB_NAME,
-                            user=DB_USER,
-                            password=DB_PASSWORD,
-                            port=DB_PORT)
+    def close_db_connection(self) -> None:
+        """Closes connection to the database"""
 
-    CUR = CONN.cursor()
+        if self.db_cursor is not None:
+            self.db_cursor.close()
+            self.db_cursor = None
 
-def close_db_connection():
-    """Closes connection to the database
+        if self.connect_to_db is not None:
+            self.connect_to_db.close()
+            self.connect_to_db = None
 
-    Returns: None
-    """
+    def create_main_table(self) -> None:
+        """Creates a table for the user's passwords"""
 
-    global CONN, CUR
-
-    if CUR is not None:
-        CUR.close()
-        CUR = None
-
-    if CONN is not None:
-        CONN.close()
-        CONN = None
-
-def create_main_table():
-    """Creates a table for the user's passwords
-
-    Returns: None
-    """
-
-    CUR.execute(sql.SQL('''CREATE TABLE IF NOT EXISTS Passwords
-                        (id serial PRIMARY KEY, account varchar(255), 
-                        password varchar(255))'''))
-    CONN.commit()
+        self.db_cursor.execute(sql.SQL('''CREATE TABLE IF NOT EXISTS Passwords
+                            (id serial PRIMARY KEY, account varchar(255), 
+                            password varchar(255))'''))
+        self.connect_to_db.commit()
 
 
-def create_secret_word_table():
-    """Creates a table for the secret word
+    def create_secret_word_table(self) -> None:
+        """Creates a table for the secret word"""
 
-    Returns: None
-    """
+        self.db_cursor.execute(sql.SQL('''CREATE TABLE IF NOT EXISTS SecretWord
+                            (id serial PRIMARY KEY,
+                            word varchar(255),
+                            hint varchar(255))'''))
+        self.connect_to_db.commit()
 
-    CUR.execute(sql.SQL('''CREATE TABLE IF NOT EXISTS SecretWord
-                        (id serial PRIMARY KEY,
-                        word varchar(255),
-                        hint varchar(255))'''))
-    CONN.commit()
+    def insert_secret_word_and_hint(self, secret_word, user_hint) -> None:
+        """Insert secret word and a hint into a table
 
-def insert_secret_word_and_hint(secret_word, user_hint):
-    """Insert secret word and a hint into a table
-    
-    Args:
-        secret_word(str): Secret word to save.
-        user_hint(str): Hint for the secret word.
+        Args:
+            secret_word(str): Secret word to save.
+            user_hint(str): Hint for the secret word.
+        """
+        self.db_cursor.execute('''INSERT INTO SecretWord (word, hint)
+                            VALUES (%s, %s) ''',
+                            (secret_word, user_hint))
+        self.connect_to_db.commit()
 
-    Returns: None
-    """
-    CUR.execute('''INSERT INTO SecretWord (word, hint)
-                           VALUES (%s, %s) ''',
-                           (secret_word, user_hint))
-    CONN.commit()
+    def insert_account_and_password(self, account, password) -> None:
+        """Insert account and a password into a table
 
-def insert_account_and_password(account, password):
-    """Insert account and a password into a table
-
-    Args:
-        account (str): The account to save.
-        password(str): Password for the account.
-
-    Returns: None
-    """
-    CUR.execute('''INSERT INTO Passwords (account, password)
-                           VALUES (%s, %s) ''',
-                           (account, password))
-    CONN.commit()
+        Args:
+            account (str): The account to save.
+            password(str): Password for the account.
+        """
+        self.db_cursor.execute('''INSERT INTO Passwords (account, password)
+                            VALUES (%s, %s) ''',
+                            (account, password))
+        self.connect_to_db.commit()
 
 
-def select_password_from_db(account):
-    """Selects a password for the particular account
+    def select_password_from_db(self, account) -> list:
+        """Selects a password for the particular account
 
-    Args:
-        account (str): The account for which to retrieve password information.
+        Args:
+            account (str): The account for which to retrieve password information.
 
-    Returns:
-        list: A list of tuples containing the 'account' and 'password' values for the given account.
-    """
+        Returns:
+            list: A list of tuples containing the 'account' and 'password' values.
+        """
 
-    CUR.execute(sql.SQL('''SELECT account,password
-                        FROM Passwords
-                        WHERE account={}''').format(sql.Identifier(account)))
-    rows = CUR.fetchall()
-    return rows
+        self.db_cursor.execute('''SELECT account,password
+                            FROM Passwords
+                            WHERE account=%s''', (account,))
+        rows = self.db_cursor.fetchall()
+        return rows
 
 
-def select_hint_from_db():
-    """Selects a hint from the SecretWord table
+    def select_hint_from_db(self) -> list:
+        """Selects a hint from the SecretWord table
 
-    Returns:
-        list: A list of tuples containing the 'hint' values.
-    """
+        Returns:
+            list: A list of tuples containing the 'hint' values.
+        """
 
-    CUR.execute(sql.SQL('''SELECT hint FROM SecretWord'''))
-    rows = CUR.fetchall()
-    return rows
+        self.db_cursor.execute(sql.SQL('''SELECT hint FROM SecretWord'''))
+        rows = self.db_cursor.fetchall()
+        return rows
 
-def select_secret_word_from_db():
-    """Selects a secret word from the SecretWord table
+    def select_secret_word_from_db(self) -> list:
+        """Selects a secret word from the SecretWord table
 
-    Returns:
-        list: A list of tuples containing the 'word' values.
-    """
+        Returns:
+            list: A list of tuples containing the 'word' values.
+        """
 
-    CUR.execute(sql.SQL('''SELECT word FROM SecretWord'''))
-    rows = CUR.fetchall()
-    return rows
+        self.db_cursor.execute(sql.SQL('''SELECT word FROM SecretWord'''))
+        rows = self.db_cursor.fetchall()
+        return rows
 
-def check_if_secret_table_exists():
-    try:
-        connect_to_db()
-        select_hint_from_db()
-        return True
-    except psycopg2.errors.UndefinedTable:
-        return False
+    def check_if_secret_table_exists(self) -> bool:
+        """Returns True if secret_table exists otherwise False"""
+
+        try:
+            self.select_hint_from_db()
+            return True
+        except psycopg2.errors.UndefinedTable:
+            return False
