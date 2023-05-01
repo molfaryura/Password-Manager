3ed3c4be622343c83e28e96bf48c2080972e20a8
molfaryura <oleksandrr1996@protonmail.com>
2023-05-01 13:49:57 +0300

    Issue #9: Fixed code style issues

--- password_manager.py
+++ password_manager.py
@@ -2,7 +2,7 @@
 
 import hashlib
 
-from tkinter import *
+from tkinter import Tk, Canvas, PhotoImage, Label, END
 from tkinter import messagebox
 
 from psycopg2 import errors
@@ -32,10 +32,54 @@ def generate_message(account: list):
 
 
 class PasswordManager():
+    """A password manager GUI application that stores account and password information in the db.
+
+    Attributes:
+    -----------
+    window: trkinter.Tk
+        the main window of the application.
+    main_widgets: MainWidgets
+        a collection of UI widgets for the main screen.
+    data_base: PasswordManagerDatabase
+        a database that stores the account and password information.
+    main_buttons: Buttons
+        a collection of UI buttons for the main screen.
+    secret_word_buttons: SecretWordUi
+        a collection of UI buttons for the secret word input screen.
+
+    Methods:
+    -----------
+    create_main_widgets():
+        Creates the main widgets of the GUI for account and password input.
+    pressed_add_secret_word():
+        Saves the secret word and hint to the database and creates the main widgets.
+    pressed_hint_button():
+        Shows the hint associated with the secret word.
+    pressed_add_button():
+        Saves the account and password to the database if the secret word is correct.
+    pressed_password_button():
+        Generates a random password and inserts it into the password entry.
+    save_secrete_word_and_hint():
+        Hashes and saves the secret word and hint to the database.
+    is_secret_word_match() -> bool:
+        Compares the hashed secret word entered by the user to the one in the database.
+    check_secret_table():
+        Checks if the secret word table exists in the database and displays the corresponding GUI.
+    show_password():
+        Retrieves and shows the password for the selected account.
+    config_main_buttons():
+        Configures the command functions for the main buttons of the GUI.
+    clear_entry():
+        Clears the input fields of the main widgets.
+    on_closing():
+        Closes the database connection and destroys the Tkinter window.
+    """
+
     main_buttons = None
     secret_word_buttons = None
 
     def __init__(self) -> None:
+        """Initializes the Tkinter window and database connection."""
         self.window = Tk()
         self.window.title('Password Manager')
         self.window.config(padx=10, pady=10, bg=BG_COLOR)
@@ -47,7 +91,7 @@ class PasswordManager():
 
         self.main_widgets = None
 
-        self.db = PasswordManagerDatabase()
+        self.data_base = PasswordManagerDatabase()
         self.check_secret_table()
 
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -55,6 +99,8 @@ class PasswordManager():
         self.window.mainloop()
 
     def create_main_widgets(self):
+        """Creates the main widgets of the GUI for account and password input."""
+
         secret_word_label = Label(text='Secret Word:', bg=BG_COLOR, font=('Arial', 12, 'bold'))
         secret_word_label.grid(column=0, row=1)
 
@@ -74,6 +120,8 @@ class PasswordManager():
 
 
     def pressed_add_secret_word(self):
+        """Saves the secret word and hint to the database and creates the main widgets."""
+
         self.save_secrete_word_and_hint()
 
         PasswordManager.secret_word_buttons.add_secret_word_button.destroy()
@@ -85,21 +133,27 @@ class PasswordManager():
         self.create_main_widgets()
 
     def pressed_hint_button(self):
-        hint = self.db.select_hint_from_db()
+        """Shows the hint associated with the secret word."""
+
+        hint = self.data_base.select_hint_from_db()
         messagebox.showinfo(message=hint)
 
     def pressed_add_button(self):
+        """Saves the account and password to the database if the secret word is correct."""
+
         if self.is_secret_word_match():
-            self.db.create_main_table()
+            self.data_base.create_main_table()
             account = self.main_widgets.account_entry.get()
             password = self.main_widgets.password_entry.get()
-            self.db.insert_account_and_password(account, password)
+            self.data_base.insert_account_and_password(account, password)
             messagebox.showinfo(title='Success',
                                 message=('Data has been saved successfully.'))
 
         self.clear_entry()
 
     def pressed_password_button(self):
+        """Generates a random password and inserts it into the password entry."""
+
         self.main_widgets.password_entry.delete(0, END)
         number = int(self.main_widgets.spinner.get())
         password = PassWord(number).password
@@ -107,23 +161,27 @@ class PasswordManager():
 
 
     def save_secrete_word_and_hint(self):
+        """Hashes and saves the secret word and hint to the database."""
+
         secret_word = hashlib.sha256(self.secret_word_buttons.add_secret_word_entry.get().encode())
         secret_word = secret_word.hexdigest()
         hint = self.secret_word_buttons.add_hint_entry.get()
 
         try:
-            self.db.create_secret_word_table()
+            self.data_base.create_secret_word_table()
         except errors.InFailedSqlTransaction:
-            self.db.connect_to_db.rollback()
+            self.data_base.connect_to_db.rollback()
         finally:
-            self.db.create_secret_word_table()
+            self.data_base.create_secret_word_table()
 
-        self.db.insert_secret_word_and_hint(secret_word, hint)
+        self.data_base.insert_secret_word_and_hint(secret_word, hint)
 
     def is_secret_word_match(self) -> bool:
+        """Compares the hashed secret word entered by the user to the one in the database."""
+
         user_secret_word = hashlib.sha256(self.main_widgets.secret_word_entry.get().encode())
         user_secret_word = user_secret_word.hexdigest()
-        result = user_secret_word == self.db.select_secret_word_from_db()[0][0]
+        result = user_secret_word == self.data_base.select_secret_word_from_db()[0][0]
         if result:
             return True
         messagebox.showerror(title='Error', message=('Incorrect secret word!'))
@@ -131,9 +189,11 @@ class PasswordManager():
 
 
     def check_secret_table(self):
-        self.db.start_db_connection()
+        """Checks if the secret word table exists in the database and displays the GUI."""
 
-        if not self.db.check_if_secret_table_exists():
+        self.data_base.start_db_connection()
+
+        if not self.data_base.check_if_secret_table_exists():
             PasswordManager.secret_word_buttons = SecretWordUi(bg_color=BG_COLOR)
             PasswordManager.secret_word_buttons.secret_word_buttons()
             PasswordManager.secret_word_buttons.grid_items()
@@ -144,9 +204,11 @@ class PasswordManager():
 
 
     def show_password(self):
+        """Retrieves and shows the password for the selected account."""
+
         if self.is_secret_word_match():
             account_name = self.main_widgets.account_entry.get()
-            result = self.db.select_password_from_db(account_name)
+            result = self.data_base.select_password_from_db(account_name)
             if result:
                 message = generate_message(result)
                 messagebox.showinfo(message=message)
@@ -156,6 +218,8 @@ class PasswordManager():
         self.clear_entry()
 
     def config_main_buttons(self):
+        """Configures the command functions for the main buttons of the GUI."""
+
         PasswordManager.main_buttons.hint_button.config(command=self.pressed_hint_button)
         search_button = PasswordManager.main_buttons.search_button
         search_button.config(command=self.show_password)
@@ -165,11 +229,17 @@ class PasswordManager():
 
 
     def clear_entry(self):
+        """Clears the input fields of the main widgets."""
+
         self.main_widgets.account_entry.delete(0, END)
         self.main_widgets.password_entry.delete(0, END)
         self.main_widgets.secret_word_entry.delete(0, END)
 
     def on_closing(self):
+        """Closes the database connection and destroys the Tkinter window
+        when the user closes the application.
+        """
+
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.db.close_db_connection()
+            self.data_base.close_db_connection()
             self.window.destroy()
